77c90031c3a336bdf42b221216d218216f2d54ce
miguel <micky.ale7@gmail.com>
2020-06-22 17:27:53 -0400

    Migraciones realizadas para implementar el nuevo property 'ImageUrl'

--- MusicStoreAPI/MusicStoreAPI/Migrations/20200621011133_NewColumnImageUrl.Designer.cs
+++ MusicStoreAPI/MusicStoreAPI/Migrations/20200621011133_NewColumnImageUrl.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MusicStoreAPI.Data;
 
 namespace MusicStoreAPI.Migrations
 {
     [DbContext(typeof(MusicStoreDbContext))]
-    partial class MusicStoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200621011133_NewColumnImageUrl")]
+    partial class NewColumnImageUrl
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
