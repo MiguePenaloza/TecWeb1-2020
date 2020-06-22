cb6fa354939817d4a347efe8f055c4f6af5babe7
miguel <micky.ale7@gmail.com>
2020-06-22 17:17:45 -0400

    Se dio diseno a la visualizacion de los 'stores'

--- Angular/MusicStoreAPP/src/app/components/store-item/store-item.component.ts
+++ Angular/MusicStoreAPP/src/app/components/store-item/store-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Store } from 'src/app/models/Srore';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-store-item',
@@ -10,9 +11,12 @@ export class StoreItemComponent implements OnInit {
 
   @Input() storeInput :Store;
   
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
   }
 
+  onEdit(store :Store) {
+    this.router.navigateByUrl(`/stores/${store.id}`);
+  }
 }
