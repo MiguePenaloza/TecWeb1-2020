b5d3e8f59fd70482288e04e04d0c813fbede71d0
miguel <micky.ale7@gmail.com>
2020-06-22 19:31:16 -0400

    Se agrego e implemento el servio para los instrumentos

---
+++ Angular/MusicStoreAPP/src/app/services/instrument.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Instrument } from '../models/Instrument';
+import { Observable } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InstrumentService {
+
+  storesUrl :string = 'https://localhost:44317/api/Stores';
+  instrumentsQueryParam :string = '?orderBy=description';
+
+  constructor(private http :HttpClient) { }
+
+  getInstruments(storeId :string) :Observable<Instrument[]> {
+    return this.http.get<Instrument[]>(`${this.storesUrl}/${storeId}/instruments${this.instrumentsQueryParam}`);
+  }
+
+  getInstrument(storeId :string, instrumentId :string) :Observable<Instrument> {
+    return this.http.get<Instrument>(`${this.storesUrl}/${storeId}/instruments/${instrumentId}`);
+  }
+
+  addInstrument(storeId :string, instrumentToAdd :Instrument) :Observable<Instrument> {
+    return this.http.post<Instrument>(`${this.storesUrl}/${storeId}/instruments/`, instrumentToAdd, httpOptions);
+  }
+
+  updateInstrument(storeId :string, instrumentId :string, instrumentToUpdate :Instrument) :Observable<Instrument> {
+    return this.http.put<Instrument>(`${this.storesUrl}/${storeId}/instruments/${instrumentId}`, instrumentToUpdate, httpOptions);
+  }
+
+  deleteInstrument(storeId :string, instrumentToDelete :Instrument) :Observable<boolean> {
+    return this.http.delete<boolean>(`${this.storesUrl}/${storeId}/instruments/${instrumentToDelete.id}`);
+  }
+}
