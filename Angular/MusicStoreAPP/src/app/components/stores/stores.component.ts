e8414b4396297878596c6b29e4ac075614dd28c6
miguel <micky.ale7@gmail.com>
2020-06-23 00:28:47 -0400

    Se agrego delete store con su boton

--- Angular/MusicStoreAPP/src/app/components/stores/stores.component.ts
+++ Angular/MusicStoreAPP/src/app/components/stores/stores.component.ts
@@ -9,14 +9,26 @@ import { StoreService } from 'src/app/services/store.service';
 })
 export class StoresComponent implements OnInit {
 
-  storesList :Store[];
+  storesList: Store[];
 
-  constructor(private storeService :StoreService) { }
+  constructor(private storeService: StoreService) { }
 
   ngOnInit(): void {
-    this.storeService.getStores().subscribe( stores => {
+    this.storeService.getStores().subscribe(stores => {
       this.storesList = stores;
     });
   }
 
+  deleteStore(storeToDelete: Store): void {
+
+    if (confirm('Are you sure you want to delete this Store?!')) {
+      this.storesList = this.storesList.filter(stores => stores.id != storeToDelete.id);
+      //delete from backend
+      this.storeService.deleteStore(storeToDelete).subscribe(r => {
+        console.log(r);
+      });
+    } else {
+      console.log("You cancel the action");
+    }
+  }
 }
