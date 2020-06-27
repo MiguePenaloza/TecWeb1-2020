35d7fdf43cf2c20e230e2f391487df0db747900e
miguel <micky.ale7@gmail.com>
2020-06-27 00:52:03 -0400

    Cambios menores en el addStore

--- Angular/MusicStoreAPP/src/app/components/add-store/add-store.component.ts
+++ Angular/MusicStoreAPP/src/app/components/add-store/add-store.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from 'src/app/models/Srore';
 
 @Component({
   selector: 'app-add-store',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddStoreComponent implements OnInit {
 
-  constructor() { }
+  newStore :Store;
+
+  constructor() { 
+    this.newStore = new Store();
+  }
 
   ngOnInit(): void {
   }
 
+  onCancel() {
+    // this.storeService.getStore(this.storeId).subscribe( s => {
+    //   this.store = s;
+    // });
+    
+    // document.getElementById("response").innerHTML = "";
+    // console.log(this.store);
+    // console.log(this.originalStore);
+  }
+
+  onSubmit() {
+  //   this.storeService.updateStore(updatedStore).subscribe( response => {
+  //     if (response) {
+  //       document.getElementById("response").innerHTML = "<h5>Cambios Guardados!!</h5>";
+  //     }
+  //   })
+  //   this.originalStore = this.store;
+  console.log(this.newStore);
+  
+  }
 }
